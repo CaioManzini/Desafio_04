2bd2f7e5a08b068ea6381fa50aebbfa72ef5611c
Caio_Manzini <kaiomnzn@gmail.com>
2022-01-31 01:23:19 -0300

    .

--- Aluno.cs
+++ Aluno.cs
@@ -12,5 +12,15 @@ namespace Desafio_04
         public int idade { get; set; }
         public double nota { get; set; }
 
+        public void Nota()
+        {
+            Aluno[] aluno =
+            {
+                new Aluno{nome = "Santos", idade = 14,nota = 8},
+                new Aluno{nome = "Silva", idade = 12, nota = 6},
+                new Aluno{nome = "pereira", idade = 13, nota = 9},
+                new Aluno{nome = "Santana", idade = 11, nota = 5}
+            };
+        }
     }
 }
