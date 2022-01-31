6b164bb1388479a784f0a55bcc2c19605f69f1fe
Caio_Manzini <kaiomnzn@gmail.com>
2022-01-31 00:45:19 -0300

    Lista aluno contendo nomes, idades, notas e as somas das notas.

---
+++ Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_04
+{
+    class Aluno
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public double nota { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> aluno = new List<Aluno>();
+            aluno.Add(new Aluno { nome = "Santos", idade = 14, nota = 8 });
+            aluno.Add(new Aluno { nome = "Silva", idade = 12, nota = 6 });
+            aluno.Add(new Aluno { nome = "Pereira", idade = 13, nota = 9 });
+            aluno.Add(new Aluno { nome = "Santana", idade = 11, nota = 5 });
+
+            foreach (var estudante in aluno)
+            {
+                Console.WriteLine((" " + estudante.nome + "\t"));
+                Console.WriteLine((" Idade: " + estudante.idade + "\t"));
+
+                Console.WriteLine(" Nota final: " + estudante.nota + "\t" + "\n");
+
+            }
+            int[] notas = { 8, 6, 9, 5 };
+            int soma = notas.Sum();
+            Console.WriteLine(" A Soma das notas: " + soma);
+        }
+    }
+}
